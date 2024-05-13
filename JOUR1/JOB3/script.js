3c9c747ef7943ff8a024b85cee5d64117663e61e
maily-vannobel <maily.vannobel@laplateforme.io>
2024-05-13 10:22:13 +0200

    dépot runtrack node job1

---
+++ JOUR1/JOB3/script.js
@@ -0,0 +1,14 @@
+// Basé sur JS. Node.JS est une plateform de dévelopemment opensource côté serveur- même backend pour appli web et mobiles
+// RUNTIME SERVEUR
+// entrées/sorties sont asynchrones i/o 
+// single thread non bloquant
+//constitué de modules natif apportant des fonctionnalités
+//
+//API REST, Appli en tps réels (RTA) blog reseauc sociaux, single page applications (SPA)
+// 
+
+
+//Single trhread
+
+
+// Librairies/packages disponibles ; express js --- TypeScript - next.js -- socket.io
