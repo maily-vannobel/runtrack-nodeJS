a37e74f75853ebbc038b4f0a638073eb735f288a
maily-vannobel <maily.vannobel@laplateforme.io>
2024-05-17 16:17:17 +0200

    jour5 job 1 à 5

--- JOUR5/runtrack_express/projet_web/index.js
+++ JOUR5/runtrack_express/projet_web/index.js
@@ -1,12 +1,23 @@
 // POINT D'ENTRÉE DU SITE WEB
 
 const express = require('express');
-
+//instance d'express
 const app = express();
+const path = require("path");
 
-// Définir une route de base
+// Définir route de base
 app.get('/', (req, res) => {
-  res.send('Hello');
+    res.status(200).sendFile(path.join(__dirname, "views", "index.html"));  });
+
+
+// route presentation (ABOUT)
+app.get("/about", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+//middleware
+app.use(function (req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 const port = 80;
